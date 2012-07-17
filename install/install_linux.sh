f89a48a49f2b91e36afc9bff3f1dbbc85894d3db
Andrew Kennedy <akennedy2292@gmail.com>
2012-07-16 20:33:31 -0400

    lolol

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -4,8 +4,8 @@ rm -rf ~/.vim
 rm ~/.gitconfig
 
 echo "Symlinking files"
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/.vim ~/.vim
 ln -s ~/dotfiles/github/.gitconfig ~/.gitconfig
 
 echo "All done."
