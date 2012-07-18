2b75a5ccbdaf3c14280734deb770b6e77174cda6
Andrew Kennedy <akennedy2292@gmail.com>
2012-07-18 09:55:08 -0400

    added zsh into install file

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -1,11 +1,29 @@
+#!/bin/sh
+
+echo "Is zsh installed? [ enter 1 for yes ]"
+read zsh
+
+
 echo "Deleting the old files"
+rm ~/.bashrc
 rm ~/.vimrc
 rm -rf ~/.vim
 rm ~/.gitconfig
+if [ "$zsh" ]
+then
+  rm ~/.zshrc
+fi
+
 
 echo "Symlinking files"
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/vim/.vim ~/.vim
 ln -s ~/dotfiles/github/.gitconfig ~/.gitconfig
+if [ "$zsh" ]
+then
+  ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+fi
+
 
 echo "All done."
