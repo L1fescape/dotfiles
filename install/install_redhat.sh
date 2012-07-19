53a93fc2024fcdd32e05ea3c3ebdc92f7c828c22
Andrew Kennedy <akennedy2292@gmail.com>
2012-07-19 14:35:40 -0400

    added redhat install script and moved install_linux.sh to install_debian.sh

---
+++ install/install_redhat.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+echo "Is zsh installed? [ y/N ]"
+read zsh
+
+case "$zsh" in
+  "y"|"Y"|"yes"|"Yes"|"YES")
+    zsh=1
+    ;;
+  *)
+    echo "Do you want to install zsh? [ Y/n ]"
+    read install_zsh
+    case "$install_zsh" in
+      "n"|"N"|"no"|"No"|"NO")
+        zsh=0
+        ;;
+      *)
+        yum install zsh
+        wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+        echo "Set zsh as default shell? [ Y/n ]"
+        read zsh_default
+        case "$zsh_default" in
+          "n"|"N"|"no"|"No"|"NO")
+            ;;
+           *)
+              chsh -s /bin/zsh
+        esac
+        zsh=1
+    esac
+esac
+
+
+echo "Deleting the old files"
+rm ~/.bashrc
+rm ~/.vimrc
+rm -rf ~/.vim
+rm ~/.gitconfig
+if [ "$zsh" ]
+then
+  rm ~/.zshrc
+  rm ~/.oh-my-zsh/themes/andrewk.zsh-theme
+fi
+
+
+echo "Symlinking files"
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/.vim ~/.vim
+ln -s ~/dotfiles/github/.gitconfig ~/.gitconfig
+if [ "$zsh" ]
+then
+  ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+  ln -s ~/dotfiles/zsh/themes/andrewk.zsh-theme ~/.oh-my-zsh/themes/andrewk.zsh-theme
+fi
+
+
+echo "All done."
