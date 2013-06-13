131c6d3b587ad5dd44aff8a453b5c60a0063148a
Andrew Kennedy <akennedy2292@gmail.com>
2013-06-13 15:25:22 -0400

    renamed dot files. symlinks still work fine via install script

--- install.sh
+++ install.sh
@@ -89,26 +89,26 @@ fi
 
 # Symbolic link files in dotfiles dir to home folder
 printf "\e[4m%s\e[0m\n" "Symlinking new files"
-ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/.vim ~/.vim
-ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/.aliases ~/.aliases
-ln -s ~/dotfiles/zsh/.functions ~/.functions
-ln -s ~/dotfiles/zsh/.paths ~/.paths
-ln -s ~/dotfiles/zsh/.bindings ~/.bindings
+ln -s ~/dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/vim ~/.vim
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/aliases ~/.aliases
+ln -s ~/dotfiles/zsh/functions ~/.functions
+ln -s ~/dotfiles/zsh/paths ~/.paths
+ln -s ~/dotfiles/zsh/bindings ~/.bindings
 ln -s ~/dotfiles/zsh/themes/andrewk.zsh-theme ~/.oh-my-zsh/themes/andrewk.zsh-theme
-ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 if [ "$os" = "o" ]
 then
-  ln -s ~/dotfiles/osx/.osx ~/
-  ln -s ~/dotfiles/osx/.hushlogin ~/
+  ln -s ~/dotfiles/osx/osx ~/.osx
+  ln -s ~/dotfiles/osx/hushlogin ~/.hushlogin
   # Copy iterm2 settings
   cp ~/dotfiles/osx/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/
   # Load osx global settings
-  source ~/dotfiles/osx/.osx-init
+  source ~/dotfiles/osx/osx-init
 fi
 
 
